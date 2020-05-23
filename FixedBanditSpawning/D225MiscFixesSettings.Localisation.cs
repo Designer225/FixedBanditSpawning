38c420165b3934d39baac03c930fdba16936bfc4
Designer225 <designer2553@gmail.com>
2020-05-22 17:28:50 -0700

    Updated localization to be more in line with localization from Battle Regeneration

--- FixedBanditSpawning/D225MiscFixesSettings.Localisation.cs
+++ FixedBanditSpawning/D225MiscFixesSettings.Localisation.cs
@@ -9,19 +9,19 @@ namespace FixedBanditSpawning
 {
     public partial class D225MiscFixesSettings
     {
-        private const string D225MiscFixesModNameText = "{=D225MiscFixesModNameText}Designer225's Miscellaneous Fixes",
+        private const string ModNameText = "{=D225MiscFixes_ModNameText}Designer225's Miscellaneous Fixes",
 
-            PatchBanditSpawningName = "{=PatchBanditSpawningName}Patch Bandit Spawning",
-            PatchBanditSpawningHint = "{=PatchBanditSpawningHint}Enable this to patch bandit spawning to allow more than 3 types of troops. Disable if another mod also does this.",
-            PatchAgentSpawningName = "{=PatchAgentSpawningName}Patch Agent Spawning",
-            PatchAgentSpawningHint = "{=PatchAgentSpawningHint}Enable this to bypass age check that makes underage agents look adult-ish. Disable if another mod also does this.",
-            PatchInvincibleChildrenName = "{=PatchInvincibleChildrenName}Patch Invincible Children",
-            PatchInvincibleChildrenHint = "{=PatchInvincibleChildrenHint}Enable this to make children less overpowered. Disable if another mod also does this.",
-            PatchWandererSpawningName = "{=PatchWandererSpawningName}Patch Wander Spawning",
-            PatchWandererSpawningHint = "{=PatchWandererSpawningHint}Enable this to get rid of the arbitrary minimum of 20 years for wanderer spawning and also to increase the age range to from adult age to adult age + 32. Disable if another mod also does this.",
-            WanderSpawningRngMaxName = "{=WanderSpawningRngMaxName}Wanderer Max RNG Age Increase",
-            WanderSpawningRngMaxHint = "{=WanderSpawningRngMaxHint}Sets the maximum age increase of wanderers during game start. Default is 32 years (originally 5 + randomized max 27). Does not require restart unlike other options.";
+            PatchBanditSpawningName = "{=D225MiscFixes_PatchBanditSpawningName}Patch Bandit Spawning",
+            PatchBanditSpawningHint = "{=D225MiscFixes_PatchBanditSpawningHint}Enable this to patch bandit spawning to allow more than 3 types of troops. Disable if another mod also does this.",
+            PatchAgentSpawningName = "{=D225MiscFixes_PatchAgentSpawningName}Patch Agent Spawning",
+            PatchAgentSpawningHint = "{=D225MiscFixes_PatchAgentSpawningHint}Enable this to bypass age check that makes underage agents look adult-ish. Disable if another mod also does this.",
+            PatchInvincibleChildrenName = "{=D225MiscFixes_PatchInvincibleChildrenName}Patch Invincible Children",
+            PatchInvincibleChildrenHint = "{=D225MiscFixes_PatchInvincibleChildrenHint}Enable this to make children less overpowered. Disable if another mod also does this.",
+            PatchWandererSpawningName = "{=D225MiscFixes_PatchWandererSpawningName}Patch Wander Spawning",
+            PatchWandererSpawningHint = "{=D225MiscFixes_PatchWandererSpawningHint}Enable this to get rid of the arbitrary minimum of 20 years for wanderer spawning and also to increase the age range to from adult age to adult age + 32. Disable if another mod also does this.",
+            WanderSpawningRngMaxName = "{=D225MiscFixes_WanderSpawningRngMaxName}Wanderer Max RNG Age Increase",
+            WanderSpawningRngMaxHint = "{=D225MiscFixes_WanderSpawningRngMaxHint}Sets the maximum age increase of wanderers during game start. Default is 32 years (originally 5 + randomized max 27). Does not require restart unlike other options.";
 
-        private static readonly TextObject ModNameTextObject = new TextObject(D225MiscFixesModNameText);
+        private static readonly TextObject ModNameTextObject = new TextObject(ModNameText);
     }
 }
